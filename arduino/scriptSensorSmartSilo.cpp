a434d088d410f7d785d7f2590b9fefbeab531005
Gabriel <gabriel@MacBook-Air-de-Gabriel.local>
2026-03-11 15:18:09 -0300

    upd

--- arduino/scriptSensorSmartSilo.cpp
+++ arduino/scriptSensorSmartSilo.cpp
@@ -1,14 +1,12 @@
-
-float tempo;       
-float distancia=0; 
+// Declarando as variáveis
+float tempo;        // A variável tempo recebe o tempo que o som demora para ir e voltar.
+float distancia=0;  // Declarando a variável que vai receber a distância
 int trig=9;
 int echo=10;
-int verm=13;
-int ama=12;
-int ver=11;
 
 
-float alturaSilo = 200;
+
+float alturaSilo = 400;
 float raioSilo = 50;
 float areaCirculo = (raioSilo*raioSilo)* 3.14;
 float volumeTotal = areaCirculo * alturaSilo;
@@ -27,24 +25,23 @@ float distanciaMaxima = alturaSilo - (alturaSilo * percentualMinimo / 100);
 
 void setup() {
   Serial.begin(9600);
-  pinMode(trig, OUTPUT);  
-  pinMode(echo, INPUT);   
-  pinMode (verm, OUTPUT);
-  pinMode (ama, OUTPUT);
-  pinMode (ver, OUTPUT);
+  pinMode(trig, OUTPUT);  //Informando que o pino é de saída
+  pinMode(echo, INPUT);   //Informando que o pino é de entrada
+ 
  }
 
 void loop() {
   
-  digitalWrite(trig, LOW);          
-  delayMicroseconds(2);             
-  digitalWrite (trig, HIGH);        
-  delayMicroseconds(10);            
-  digitalWrite (trig, LOW);         
+  digitalWrite(trig, LOW);          //Desligando o envio de som
+  delayMicroseconds(2);             //Tempo que vai ficar desligado
+  digitalWrite (trig, HIGH);        //Liga o envio de som
+  delayMicroseconds(10);            //Tempo que vai ficar ligado
+  digitalWrite (trig, LOW);         //Desligando o envio de som
   
-  tempo=pulseIn(echo, HIGH);        
+  tempo=pulseIn(echo, HIGH);        // A variável "TEMPO" recebe o tempo que o som demora para ir e voltar de um obstaculo
   
-
+  //Como o tempo está dobrado, pois é o tempo de ir e voltar, dividimos por 2
+  //Velocidade do som aproximadamente 0.0343 centimetro por microsegundos
   distancia= (tempo/2)*0.0343 ;   
   
   float volumeOcupado = areaCirculo * (alturaSilo - distancia);
@@ -52,38 +49,23 @@ void loop() {
   
   
 
-	
-	
-  	
-  	Serial.println(percentualOcupado);
+	//Serial.print("Volume : ");
+	//Serial.print(volumeOcupado);
+  	//Serial.println(" centimetros cubicos");
+  	Serial.print(percentualOcupado);
+    Serial.println("%");
   
   
   
-
+  // Se a distância for menor que 10cm os três LED pisca
   if (distancia<=distanciaMinima){
-    digitalWrite (ver, LOW);
-    digitalWrite (ama, LOW);
-    
-    digitalWrite (verm, HIGH);
-    delay(500);
-    
-    digitalWrite (verm, LOW);
-    delay (500);
+    Serial.println("SILO ACIMA DO LIMITE, VOCÊ PRECISA ENSVAZIAR");
   }else if(distancia <=distanciaMaxima){
-    digitalWrite (ver, HIGH);
-    digitalWrite (verm, LOW);
-    digitalWrite (ama, LOW);
-    delay (500);
+    Serial.println("SILO DENTRO DO LIMITE");
   }else{
-    digitalWrite (ama, HIGH);
-    digitalWrite (ver, LOW);
-    digitalWrite (verm, LOW);
-    delay (500);
-
-    digitalWrite (ama, LOW);
-    delay (500);    
+    Serial.println("SILO ABAIXO DO LIMITE, VOCÊ PRECISA ENCHER");
   }
   
-  
+  delay(1000);
     
 }
